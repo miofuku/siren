9ec57bd1a3dad3c2f8f8b5b774ae692a9c0ba576
Bijun Li <bijunli86@gmail.com>
2024-09-25 21:34:43 +0200

    Use google maps

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,17 @@
 'use client';
 
 import React from 'react';
-import Map, { Marker } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 37.7749, // San Francisco latitude
+  lng: -122.4194 // San Francisco longitude
+};
 
 interface MapProps {
   posts: Array<{
@@ -16,28 +25,43 @@ interface MapProps {
 }
 
 const MapComponent: React.FC<MapProps> = ({ posts }) => {
-  return (
-    <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: '100%', height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
     >
       {posts.map((post) => (
         <Marker
           key={post.id}
-          longitude={post.location.coordinates[0]}
-          latitude={post.location.coordinates[1]}
-        >
-          <div className="marker">{post.title}</div>
-        </Marker>
+          position={{
+            lat: post.location.coordinates[1],
+            lng: post.location.coordinates[0]
+          }}
+          title={post.title}
+        />
       ))}
-    </Map>
-  );
+    </GoogleMap>
+  ) : <></>
 };
 
-export default MapComponent;
+export default React.memo(MapComponent);
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   },
 }
 
